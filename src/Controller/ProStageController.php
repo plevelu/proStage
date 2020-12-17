5ef69bf090ec9c2abaf45c8d1fa3cf6a8231908e
Levelu Pierre <plevelu@iutbayonne.univ-pau.fr>
2020-12-17 15:06:11 +0100

    ajout route dynamic pour stge

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,19 +8,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStageController extends AbstractController
 {
+    /**
+     * @Route("/", name="pro_stage_accueil")
+    */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig');
     }
+
     /**
-    * @Route("/entreprises", name="proStage/entreprises")
+     * @Route("/entreprises", name="proStage_entreprises")
     */
     public function afficherEntreprises(): Response
     {
-        return $this->render('pro_stage/affichageEntreprises.twig');
+        return $this->render('pro_stage/affichageEntreprises.html.twig');
     }
+
+    /**
+     * @Route("/formations", name="proStage_formations")
+    */
     public function afficherFormations(): Response
     {
-        return $this->render('pro_stage/affichageFormations.twig');
+        return $this->render('pro_stage/affichageFormations.html.twig');
+    }
+
+    /**
+     * @Route("/stage/{id}", name="proStage_stages")
+    */
+    public function afficherStage($id): Response
+    {
+        return $this->render('pro_stage/stage.html.twig',['id_Stage'=>$id]);
     }
 }
