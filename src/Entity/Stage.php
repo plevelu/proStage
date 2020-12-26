f18817c1079f045ad6e962cdd54819774f4e84de
Levelu Pierre <plevelu@iutbayonne.univ-pau.fr>
2020-12-26 17:57:54 +0100

    ajout des tables par make entity

---
+++ src/Entity/Stage.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $intitule;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mission;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresseMail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIntitule(): ?string
+    {
+        return $this->intitule;
+    }
+
+    public function setIntitule(string $intitule): self
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    public function getMission(): ?string
+    {
+        return $this->mission;
+    }
+
+    public function setMission(string $mission): self
+    {
+        $this->mission = $mission;
+
+        return $this;
+    }
+
+    public function getAdresseMail(): ?string
+    {
+        return $this->adresseMail;
+    }
+
+    public function setAdresseMail(string $adresseMail): self
+    {
+        $this->adresseMail = $adresseMail;
+
+        return $this;
+    }
+}
