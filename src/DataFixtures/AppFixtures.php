2048828080fb0733d2ce0eb9d68f69d4c1cdb6bc
Levelu Pierre <plevelu@iutbayonne.univ-pau.fr>
2021-01-13 17:15:53 +0100

    fin de journée ajout de génération entreprise. reste a faire stage et lier

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,18 +5,31 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Formation;
+use App\Entity\Entreprise;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $formationIUT = new Formation();
-        $formationIUT->setNom("IUT info");
-        $formationIUT->setDescription("formation bac+2 technique en informatique");
-        $manager->persist($formationIUT);
-        $formationLiscence = new Formation();
-        $formationLiscence->setNom("license");
-        $formationLiscence->setDescription("formation bac+3");
-        $manager->persist($formationLiscence);
+        //générateur données faker
+        $faker = \Faker\Factory::create('fr_FR');
+        //génération formation
+        for ($i=1; $i<=10; $i++)
+        {
+          $formation = new Formation();
+          $formation->setNom($faker->realText($maxNbChars = 10,$indexSize = 2));
+          $formation->setDescription($faker->realText($maxNbChars = 15,$indexSize = 2));
+          $manager->persist($formation);
+        }
+        //génération entreprise
+        for ($i=1; $i<=10; $i++)
+        {
+          $entreprise = new Entreprise();
+          $entreprise->setNom($faker->realText($maxNbChars = 10,$indexSize = 2));
+          $entreprise->setActivite($faker->realText($maxNbChars = 15,$indexSize = 2));
+          $entreprise->setAdresse($faker->realText($maxNbChars = 15,$indexSize = 2));
+          $manager->persist($entreprise);
+        }
         $manager->flush();
     }
 }
