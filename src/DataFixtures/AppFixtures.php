cc89ae6b0318358c3925be5e8004ace8a292002b
Levelu Pierre <plevelu@iutbayonne.univ-pau.fr>
2021-01-15 13:48:46 +0100

    point de sauvegarde des fixtures, entreprise et formation ok, reste a ajouter stage et tout lier

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,22 +14,30 @@ class AppFixtures extends Fixture
         //générateur données faker
         $faker = \Faker\Factory::create('fr_FR');
         //génération formation
-        for ($i=1; $i<=10; $i++)
-        {
           $formation = new Formation();
-          $formation->setNom($faker->realText($maxNbChars = 10,$indexSize = 2));
-          $formation->setDescription($faker->realText($maxNbChars = 15,$indexSize = 2));
+          $formation->setNom("IUT INFO");
+          $formation->setDescription("formation technique en informatique niveau bac+2");
           $manager->persist($formation);
-        }
-        //génération entreprise
-        for ($i=1; $i<=10; $i++)
-        {
+          $formation = new Formation();
+          $formation->setNom("IUT GEEI");
+          $formation->setDescription("formation technique en génie industrielle niveau bac+2");
+          $manager->persist($formation);
+          $formation = new Formation();
+          $formation->setNom("Liscence PA");
+          $formation->setDescription("formation en informatique niveau bac+3");
+          $manager->persist($formation);
+        //génération entreprises
           $entreprise = new Entreprise();
-          $entreprise->setNom($faker->realText($maxNbChars = 10,$indexSize = 2));
-          $entreprise->setActivite($faker->realText($maxNbChars = 15,$indexSize = 2));
+          $entreprise->setNom("FuturaKode");
+          $entreprise->setActivite("Programmation web");
           $entreprise->setAdresse($faker->realText($maxNbChars = 15,$indexSize = 2));
           $manager->persist($entreprise);
-        }
+          $entreprise = new Entreprise();
+          $entreprise->setNom("Armée404");
+          $entreprise->setActivite("cyber sécurité");
+          $entreprise->setAdresse($faker->realText($maxNbChars = 15,$indexSize = 2));
+          $manager->persist($entreprise);
+
         $manager->flush();
     }
 }
